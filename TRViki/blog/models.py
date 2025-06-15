90cab05a68e2c7910fc74a68920272f98d1d8df6
Trifon Kovalenko <trifonkovalenko409@gmail.com>
2025-06-15 14:53:31 +0300

    Add File1

--- TRViki/blog/models.py
+++ TRViki/blog/models.py
@@ -1,16 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Post(models.Model):
-    '''данные о посте'''
-    title = models.CharField('Заголовок записи', max_length=100)
-    description = models.TextField('Текст записи')
-    author = models.CharField('Имя автора', max_length=100)
-    date = models.DateField('Дата публикации')
 
-    def __str__(self):
-        return f'{self.title}, {self.author}'
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
 
     class Meta:
-        verbose_name = 'Запись'
-        verbose_name_plural = 'Записи'
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
 
+    def __str__(self):
+        return self.title
