06948a67bc445b923f87c07a392a6d727d620b0d
Trifon Kovalenko <trifonkovalenko409@gmail.com>
2025-06-15 15:13:11 +0300

    15/06/2025

--- TRViki/blog/models.py
+++ TRViki/blog/models.py
@@ -5,6 +5,12 @@ from django.contrib.auth.models import User
 class Post(models.Model):
 
     class Status(models.TextChoices):
+        '''данные о посте'''
+        title = models.CharField('Заголовок записи', max_length=100)
+        description = models.TextField('Текст записи')
+        author = models.CharField('Имя автора', max_length=100)
+        date = models.DateField('Дата публикации')
+
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
@@ -13,7 +19,7 @@ class Post(models.Model):
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
-    body = models.TextField()
+
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -22,10 +28,12 @@ class Post(models.Model):
                               default=Status.DRAFT)
 
     class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
         ordering = ['-publish']
         indexes = [
             models.Index(fields=['-publish']),
         ]
 
     def __str__(self):
-        return self.title
+        return f'{self.title}, {self.author}'
